008120a6afec453e6f0dc66679ca1ce4928aa574
Nadia <nadia.shevtsova@oceanshq.com>
2023-11-27 12:58:18 +1000

    236. Adding an 'Add Place' Screen (Challenge Solution 3/6)

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,3 +1,4 @@
+import 'package:favourite_places/screens/add_place.dart';
 import 'package:flutter/material.dart';
 import 'package:favourite_places/widgets/places_list.dart';
 
@@ -5,15 +6,23 @@ class PlacesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
           title: const Text('Your places'),
           actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (ctx) => const AddPlaceScreen(),
+                  ),
+                );
+              },
+            ),
           ],
-      ),
-      body: PlacesList(
+        ),
+        body: PlacesList(
           places: [],
-        )
-    );
+        ));
   }
 }
