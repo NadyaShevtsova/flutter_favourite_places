e08b98a65d72c4890f07706fa1a4905274419871
Nadia <nadia.shevtsova@oceanshq.com>
2023-11-27 13:13:56 +1000

    237. Adding 'riverpod' & A Provider (Challenge Solution 4/6)

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favourite_places/models/place.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+
+}
