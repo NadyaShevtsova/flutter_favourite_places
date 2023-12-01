66bf122fd40b95293be545d1e4b8c37fd3314b75
Nadia <nadia.shevtsova@oceanshq.com>
2023-12-01 10:51:28 +1000

    261. Loading Data from the SQL Database

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -29,7 +29,7 @@ class PlacesScreen extends ConsumerWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.all(8),
-          child: PlacesList(
+          child: FutureBuilder PlacesList(
             places: userPlaces,
           ),
         ));
